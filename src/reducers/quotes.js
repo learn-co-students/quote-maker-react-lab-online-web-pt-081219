847d41f42c58c01221a5089cc7671a955f37f70b
github username <github email address>
2020-06-02 23:20:30 -0600

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,42 @@
 export default (state = [], action) => {
-  return state;
+  let idx;
+  let quote;
+  switch (action.type) {
+
+
+    case "ADD_QUOTE":
+      return state.concat(action.quote);
+
+    case "REMOVE_QUOTE":
+      console.log(action.id)
+      return state.filter(quote => quote.id !== action.id);
+
+    case 'UPVOTE_QUOTE':
+      console.log(action.id)
+      idx = state.findIndex(quote => quote.id === action.id);
+      quote = state[idx];
+
+      return [
+        ...state.slice(0, idx),
+        Object.assign({}, quote, { votes: quote.votes += 1 }),
+        ...state.slice(idx + 1)
+      ];
+
+    case 'DOWNVOTE_QUOTE':
+      console.log(action.id)
+      idx = state.findIndex(quote => quote.id === action.id);
+      quote = state[idx];
+      if (quote.votes > 0) {
+        return [
+          ...state.slice(0, idx),
+          Object.assign({}, quote, { votes: quote.votes -= 1 }),
+          ...state.slice(idx + 1)
+        ];
+      }
+      return state;  
+
+    default:
+      return state;
+  }
+  
 }
