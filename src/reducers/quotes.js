11fae3e636fd7e588d9d4bc0585967574862947d
Leah Silber <LeahSilber@gmail.com>
2020-06-03 14:01:05 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,37 @@
+import uuid from "uuid";
+
+const id = uuid();
+
 export default (state = [], action) => {
-  return state;
-}
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+
+    case "REMOVE_QUOTE":
+      return state.filter((quote) => {
+        return quote.id !== action.quoteId;
+      });
+
+    case "UPVOTE_QUOTE":
+      for (let quote of state) {
+        if (quote.id === action.quoteId) {
+          quote.votes += 1;
+        }
+      }
+      return state;
+
+    case "DOWNVOTE_QUOTE":
+      for (let quote of state) {
+        if (quote.id === action.quoteId) {
+          if (quote.votes === 0) {
+          } else {
+            quote.votes -= 1;
+          }
+        }
+      }
+      return state;
+
+    default:
+      return state;
+  }
+};
